6d5ad1c2f8a1d2bf3a48335b8f15a9a4118a4bc6
mavenezuela <mavenezuela@prestadores.amil.com.br>
2017-08-07 14:50:17 -0300

    Learning JWT Identity

---
+++ Models/GroceryListContext .cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace dotnet_grocery_list.Models
+{
+    public class GroceryListContext : DbContext
+    {
+        public GroceryListContext(DbContextOptions<GroceryListContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<GroceryItem> GroceryList { get; set; }
+    }
+}
+
